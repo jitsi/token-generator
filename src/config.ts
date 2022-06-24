8e8bcd4d893ad57123be42167ca6039c1e28d80b
Aaron van Meerten <aaron.van.meerten@gmail.com>
2022-06-24 11:13:22 -0400

    sort and cleanup

--- src/config.ts
+++ src/config.ts
@@ -20,49 +20,47 @@ if (result.error) {
  * Maps a public key base URL to a kidPattern, if any
  */
 export interface AsapBaseUrlMapping {
-    kid?: string,
-    baseUrl: string;
     appendKidPrefix: boolean;
+    baseUrl: string;
+    kid?: string,
 }
 
 const env = envalid.cleanEnv(process.env, {
-    LOG_LEVEL: envalid.str({ default: 'info' }),
-    PORT: envalid.num({ default: 8017 }),
-    ASAP_SIGNING_KEY_FILE: envalid.str(),
-    ASAP_JWT_KID: envalid.str({ default: '' }),
-    ASAP_JWT_ISS: envalid.str({ default: 'jitsi-token-generator' }),
     ASAP_JWT_AUD: envalid.str({ default: 'jitsi' }),
+    ASAP_JWT_ISS: envalid.str({ default: 'jitsi-token-generator' }),
+    ASAP_JWT_KID: envalid.str(),
     ASAP_JWT_SUB: envalid.str({ default: undefined }),
     ASAP_PUB_KEY_TTL: envalid.num({ default: 3600 }),
+    ASAP_SIGNING_KEY_FILE: envalid.str(),
+    HOSTNAME: envalid.str({ default: '' }),
     KEY_PREFIX_PATTERN: envalid.str({ default: '^(.*)/(.*)$' }),
+    LOG_LEVEL: envalid.str({ default: 'info' }),
+    PORT: envalid.num({ default: 8017 }),
+    PROTECTED_API: envalid.bool({ default: true }),
+    REQUEST_RETRY_COUNT: envalid.num({ default: 2 }),
+    REQUEST_TIMEOUT_MS: envalid.num({ default: 8000 }),
     SYSTEM_ASAP_BASE_URL_MAPPINGS: envalid.json(
         { example:
             '[{"kid": "kidPattern", baseUrl": "https://jaas-public-keys.jitsi.net/server/dev"}]'
         }),
-    SYSTEM_ASAP_JWT_AUD: envalid.str(),
     SYSTEM_ASAP_JWT_ACCEPTED_HOOK_ISS: envalid.str(),
-    PROTECTED_API: envalid.bool({ default: true }),
-    REQUEST_TIMEOUT_MS: envalid.num({ default: 8000 }),
-    REQUEST_RETRY_COUNT: envalid.num({ default: 2 }),
-    HOSTNAME: envalid.str({ default: '' }),
-    VOLATILE_EVENTS: envalid.bool({ default: true })
+    SYSTEM_ASAP_JWT_AUD: envalid.str()
 });
 
 export default {
-    LogLevel: env.LOG_LEVEL,
-    HTTPServerPort: env.PORT,
-    AsapSigningKeyFile: env.ASAP_SIGNING_KEY_FILE,
-    AsapJwtKid: env.ASAP_JWT_KID,
-    AsapJwtIss: env.ASAP_JWT_ISS,
     AsapJwtAud: env.ASAP_JWT_AUD,
+    AsapJwtIss: env.ASAP_JWT_ISS,
+    AsapJwtKid: env.ASAP_JWT_KID,
     AsapJwtSub: env.ASAP_JWT_SUB,
-    RequestTimeoutMs: env.REQUEST_TIMEOUT_MS,
-    RequestRetryCount: env.REQUEST_RETRY_COUNT,
-    Hostname: env.HOSTNAME,
-    VolatileEvents: env.VOLATILE_EVENTS,
-    ProtectedApi: env.PROTECTED_API,
     AsapPubKeyTTL: env.ASAP_PUB_KEY_TTL,
+    AsapSigningKeyFile: env.ASAP_SIGNING_KEY_FILE,
+    HTTPServerPort: env.PORT,
+    Hostname: env.HOSTNAME,
     KidPrefixPattern: env.KEY_PREFIX_PATTERN,
+    LogLevel: env.LOG_LEVEL,
+    ProtectedApi: env.PROTECTED_API,
+    RequestRetryCount: env.REQUEST_RETRY_COUNT,
+    RequestTimeoutMs: env.REQUEST_TIMEOUT_MS,
     SystemAsapBaseUrlMappings: env.SYSTEM_ASAP_BASE_URL_MAPPINGS,
     SystemAsapJwtAcceptedAud: env.SYSTEM_ASAP_JWT_AUD,
     SystemAsapJwtAcceptedHookIss: env.SYSTEM_ASAP_JWT_ACCEPTED_HOOK_ISS.split(',')
