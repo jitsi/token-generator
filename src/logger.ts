d2e0e31e6f74245bc51b9b0dd6da33a84c33cb5e
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-06-20 15:10:53 -0400

    task: respect env based LOG_LEVEL in logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,7 @@
 import jitsiLogger from '@jitsi/logger';
 
+jitsiLogger.setLogLevel(process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info');
+
 /**
  * An instantiated and configured {@code jitsi logger} instance.
  */
